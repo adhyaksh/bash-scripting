19e346d1d53f61289a92c9a3b1a299152a1a61ab
Vikas Gupta <vikasgupta.eng@gmail.com>
2025-01-16 16:43:20 +0530

    this is the branch createin

--- hello.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "hello bash/n"
-
-adding_string_to_number="s"
-v=$(expr 5 + $adding_string_to_number)
-
--- hello1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#comment line 1 
-echo "hello bash"
-
-#setting variable 
-var=variableValue
-
-echo "this is the variable $var"
-
